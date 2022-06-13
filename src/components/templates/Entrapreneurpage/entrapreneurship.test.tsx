11830d1ea19203935ffdb90e5e6ef2f8cef8650a
coderanuj01 <anuj.bhatt@zemosolabs.com>
2022-06-13 22:35:33 +0530

    new test added on page

--- src/components/templates/Entrapreneurpage/entrapreneurship.test.tsx
+++ src/components/templates/Entrapreneurpage/entrapreneurship.test.tsx
@@ -3,13 +3,25 @@ import { EntrepreneurshipPageComponent } from "./Entrapreneur";
 import { NavbarComponent } from "../../Organisms/Navbar/Navbar";
 import { EntrepreneurshipComponent } from "../../Organisms/Entrepreneurship/Entrepreneurship";
 import { FooterComponent } from "../../Organisms/Footer/footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 describe("Enterpreneur test", () => {
   test("test enterprenuer template", () => {
-    <EntrepreneurshipPageComponent
-      header={<NavbarComponent />}
-      body={<EntrepreneurshipComponent />}
-      footer={<FooterComponent />}
-    ></EntrepreneurshipPageComponent>;
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="*"
+            element={
+              <EntrepreneurshipPageComponent
+                header={<NavbarComponent />}
+                body={<EntrepreneurshipComponent />}
+                footer={<FooterComponent />}
+              ></EntrepreneurshipPageComponent>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    );
   });
 });
