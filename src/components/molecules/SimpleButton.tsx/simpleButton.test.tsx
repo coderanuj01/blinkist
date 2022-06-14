ee207bd35056eddb64e779302d2d232e88d8d1c3
coderanuj01 <anuj.bhatt@zemosolabs.com>
2022-06-14 07:05:57 +0530

    improving test

--- src/components/molecules/SimpleButton.tsx/simpleButton.test.tsx
+++ src/components/molecules/SimpleButton.tsx/simpleButton.test.tsx
@@ -3,12 +3,27 @@ import { ReactComponent as DropDownIcon } from "../../../Icon/dropdownarrow.svg"
 import { SimpleButtonComponent } from "./SimpleButton";
 
 describe("simpleButton test", () => {
-  it("connected rest button", () => {
+  it("connected hover button", () => {
     render(
       <SimpleButtonComponent
         children={"test content"}
         icon={<DropDownIcon />}
         buttonType="connectHover"
+      />
+    );
+
+    const simpleButtonText = screen.getByText("test content");
+    expect(simpleButtonText).toBeInTheDocument();
+  });
+});
+
+describe("simpleButton test again", () => {
+  it("connected rest button", () => {
+    render(
+      <SimpleButtonComponent
+        children={"test content"}
+        icon={<DropDownIcon />}
+        buttonType="connectRest"
       />
     );
 
